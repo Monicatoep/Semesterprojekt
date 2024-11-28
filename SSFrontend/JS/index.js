8543365d168e71454a8fbedff838f839f1d08c44
Emma Hyttel <emhyttel@gmail.com>
2024-11-28 14:14:37 +0100

    Datastrukturer og metoder til at uploade billeder

--- SSFrontend/JS/index.js
+++ SSFrontend/JS/index.js
@@ -1,12 +1,16 @@
 import SeasonList from './Components/seasonList.js';
 import SeasonFolder from './Components/seasonFolder.js';
+import addPhoto from './Components/addPhoto.js';
+import { ref } from 'vue';
+
 
 const baseUrl = "https://seasonalstoryrest.azurewebsites.net/api/photos"
 
   Vue.createApp({
     components: {
       SeasonList,
-      SeasonFolder
+      SeasonFolder, 
+      AddPhoto
     },
     data() {
       return {
@@ -18,7 +22,15 @@ const baseUrl = "https://seasonalstoryrest.azurewebsites.net/api/photos"
           { title: 'Vinter', description: '' }
         ],
         selectedSeason: null, // For storing the selected season
-        addData: {}
+        addData: { selectedFile: null, temperature: 0, season: selectedSeason },
+        temperatureIntervals: [
+          {name: "Under 0°C", value: 0},
+          {name: "0-12°C", value: 1},
+          {name: "13-18°C", value: 2},
+          {name: "19-23°C", value: 3},
+          {name: "24-30°C", value: 4},
+          {name: "Over 30°C", value: 5},
+        ]
       };
     },
     methods: {
@@ -26,11 +38,39 @@ const baseUrl = "https://seasonalstoryrest.azurewebsites.net/api/photos"
         this.selectedSeason = season;
         this.currentComponent = 'SeasonFolder';
       },
+      showAddPage() {
+        this.currentComponent = 'AddPhoto';
+      },
       goBack() {
         this.selectedSeason = null;
         this.currentComponent = 'SeasonList';
       },
-      addPhoto(){
+      async addPhoto() {
+        try {
+          this.addData.selectedFile = this.onUpload()
+          response = await axios.post(baseUrl, this.addData)
+          this.addMessage = "response " + response.status + " " + response.statusText
+        } catch (ex) {
+          alert(ex.message)
+        }
+      },
+      onFileSelected (event) {
+        this.selectedFile = event.target.files[0]
+        this.url = URL.createObjectURL(this.selectedFile)
+      },
+      onUpload () {
+        const fd = new FormData()
+        fd.append('image', this.selectedFile)
+        axios.post(baseUrl, fd, {
+          onUploadProgress: uploadEvent => {
+            console.log('Upload Progress ' + Math.round(uploadEvent.loaded / uploadEvent.total * 100) + ' %')
+          }
+        })
+          .then(res => {
+            console.log(res)
+          })
+      }, 
+      selectTemp () {
 
       }
     }
