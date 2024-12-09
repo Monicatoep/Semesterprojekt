24cc40dee565e67b2514c7ad29efb9e400949af6
FrederikkeL <fretzo@hotmail.com>
2024-12-09 11:00:41 +0100

    slideshow shown on pi on start

--- SSPi/Components/photo.js
+++ SSPi/Components/photo.js
@@ -3,26 +3,15 @@ export default {
         template:
       /*html*/
       `
-        <div>
-          
-          
-          <div class="container mt-4">
-            <div class="row">
-              <!-- Loop through filtered photos and create a card for each -->
-              <div class="col-lg-3 col-md-6 mb-4" v-for="photo in photos" :key="photo.id">
-                <div class="card">
-                  <img :src="photo.uploadedImage" class="card-img-top" alt="Photo" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+                  <img :src="selectedPhoto.uploadedImage" class="card-img-top" alt="Photo" />
+ 
       `,
   
     data() {
       return {
         photos: [], // Unfiltered photos fetched from the API
-        
+        selectedPhoto  : Object,
+        counter: 0
       };
     },
   
@@ -33,24 +22,36 @@ export default {
     methods: {
       async helperGetAndShow() {
         try {
+        this.counter = 0
           const response = await axios.get(baseUrl);
           this.photos = response.data; // Populate photos array with data from API
+          this.selectedPhoto = this.photos[0]
         } catch (ex) {
           alert(ex.message);
         }
-      }
+      },
+      slideshow(){
+        
+            this.selectedPhoto = this.photos[this.counter++]
+            if (this.counter > this.photos.length-1)
+                this.counter = 0
+            
+        }
+
+      
   
       
     },
   
     // Call getAllPhotos when the component is mounted
     mounted() {
-       
-      //const baseUrl = "http://localhost:5122/api/photos"; // Local testing, change to your own localhost
-      this.helperGetAndShow(baseUrl);
-    }
-  };
-
-
-
+        this.helperGetAndShow().then(() => {
+          if (this.photos.length > 0) {
+            setInterval(() => {
+              this.slideshow(); // Start slideshowet og skift hvert 10. sekund
+            }, 10000); // 10000 ms = 10 sekunder
+          }
+        });
+      }
+    };
 
