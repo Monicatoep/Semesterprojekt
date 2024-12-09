c8274e3eef472ca3a06d0d659f6d0012b0c54d15
NannaRister <nannarister@hotmail.com>
2024-12-09 12:12:45 +0100

    Folder card har fået samme farve som baggrunden på siden

--- SSFrontend/JS/Components/seasonList.js
+++ SSFrontend/JS/Components/seasonList.js
@@ -6,7 +6,7 @@ export default {
       <div class="row">
         <div class="col-md-6 mb-4" v-for="season in seasons" :key="season.title">
           <div class="card border-0 text-center" style="width: 100%;">
-            <div class="card-body">
+            <div class="card-body" style="background-color: #FFECC8;">
               <h5 id="title" class="card-title">{{ season.title }}</h5>
               <p class="card-text">{{ season.description }}</p>
               <img id="image" src="folderimage.png" style="cursor: pointer;" @click="$emit('show-folder', season)">
